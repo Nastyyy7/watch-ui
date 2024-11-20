866e7f661945c9dfa24011798e6dd3fd598b5e20
Nastya <isip_a.g.parshenkova@mpt.ru>
2024-11-20 19:21:55 +0300

    Начало проекта

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: '',
+    distDir: 'out',
+    output: 'export',
+    poweredByHeader: false,
+    reactStrictMode: true,
+    trailingSlash: true,
+};
+
+export default nextConfig;
